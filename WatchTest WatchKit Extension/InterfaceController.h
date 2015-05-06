3d1d55660b7e70724787ce6c364fd12c4cb2c3af
SangChan Lee <sangchan82@gmail.com>
2015-05-06 16:44:40 +0900

    add .gitignore

---
+++ WatchTest WatchKit Extension/InterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  InterfaceController.h
+//  WatchTest WatchKit Extension
+//
+//  Created by SangChan on 2015. 5. 6..
+//  Copyright (c) 2015년 sangchan. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface InterfaceController : WKInterfaceController
+
+@end
