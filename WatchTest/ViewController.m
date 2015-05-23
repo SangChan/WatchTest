43e755d6c56402a3d59d58fc0708d0e2ca6c5cc7
SangChan Lee <sangchan82@gmail.com>
2015-05-23 13:21:54 +0900

    add bg color for ios app

--- WatchTest/ViewController.m
+++ WatchTest/ViewController.m
@@ -19,6 +19,12 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     [[NSNotificationCenter defaultCenter]addObserver:self selector:@selector(changeLabel:) name:@"changeLabel" object:nil];
+    
+    CIColor *topColor = [CIColor colorWithRed:0.03 green:0.16 blue:0.31 alpha:1];
+    CIColor *bottomColor = [CIColor colorWithRed:0.49 green:0.73 blue:0.74 alpha:1];
+
+    self.view.layer.contents = (id)[self textureWithVerticalGradientofSize:CGSizeMake(self.view.frame.size.width, self.view.frame.size.height) topColor:topColor bottomColor:bottomColor].CGImage;
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -29,6 +35,22 @@
 - (void)changeLabel:(NSNotification *)notification
 {
     [self.mirrorLabel setText:[notification.userInfo objectForKey:@"randomNumber"]];
+}
+
+- (UIImage*)textureWithVerticalGradientofSize:(CGSize)size topColor:(CIColor*)topColor bottomColor:(CIColor*)bottomColor
+{
+    CIContext *coreImageContext = [CIContext contextWithOptions:nil];
+    CIFilter *gradientFilter = [CIFilter filterWithName:@"CILinearGradient"];
+    [gradientFilter setDefaults];
+    CIVector *startVector = [CIVector vectorWithX:size.width/2 Y:0];
+    CIVector *endVector = [CIVector vectorWithX:size.width/2 Y:size.height];
+    [gradientFilter setValue:startVector forKey:@"inputPoint0"];
+    [gradientFilter setValue:endVector forKey:@"inputPoint1"];
+    [gradientFilter setValue:bottomColor forKey:@"inputColor0"];
+    [gradientFilter setValue:topColor forKey:@"inputColor1"];
+    CGImageRef cgimg = [coreImageContext createCGImage:[gradientFilter outputImage]
+                                              fromRect:CGRectMake(0, 0, size.width, size.height)];
+    return [UIImage imageWithCGImage:cgimg];
 }
 
 @end
